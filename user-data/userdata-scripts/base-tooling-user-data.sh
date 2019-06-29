9b4414527dd3b0c3bae35b40b2d2d34a2c1c2645
Daniel Roach <danielroach22@gmail.com>
2019-06-30 00:45:14 +1000

    Updating userdata script

--- user-data/userdata-scripts/base-tooling-user-data.sh
+++ user-data/userdata-scripts/base-tooling-user-data.sh
@@ -4,3 +4,7 @@ yum update yum
 yum install python
 easy_install pip
 pip install ansible
+pip install awscli --upgrade --user
+export PATH=~/.local/bin:$PATH
+echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
+aws configure
