3e34199b672180f8b68c607de13d6c80349e99ac
Daniel Roach <danielroach22@gmail.com>
2019-07-07 23:33:44 +1000

    Updating with comments and playbook step

--- user-data/userdata-scripts/base-tooling-user-data.sh
+++ user-data/userdata-scripts/base-tooling-user-data.sh
@@ -19,4 +19,8 @@ echo -e "$aws_access_key \n$aws_secret_access_key \n\n" >> /tmp/aws_cli_cre.txt
 aws configure < /tmp/aws_cli_cre.txt
 rm -f /tmp/aws_cli_cre.txt
 
+#copy resources for initial setup
 aws s3 sync s3://cm22-resources /tmp/
+
+#run the setup playbook for GIT hub use
+ansible-playbook /tmp/setup-files/setup-git/setup-git.yml
