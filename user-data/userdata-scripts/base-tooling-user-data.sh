dd8b0a8629b12795fb734e10a4c1a33a01b2051b
Daniel Roach <danielroach22@gmail.com>
2019-07-08 02:00:07 +1000

    updating user data file

--- user-data/userdata-scripts/base-tooling-user-data.sh
+++ user-data/userdata-scripts/base-tooling-user-data.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-/bin/echo "Hello World" >> /tmp/userdata_testfile.txt
 
 #variables
 aws_access_key=????????????????
@@ -24,3 +23,15 @@ aws s3 sync s3://cm22-resources /tmp/
 
 #run the setup playbook for GIT hub use
 ansible-playbook /tmp/setup-files/setup-git/setup-git.yml
+
+# Create the role and attach the trust policy that allows EC2 to assume this role.
+$ aws iam create-role --role-name Test-Role-for-EC21 --assume-role-policy-document file://tmp/setup-files/iam-admin-role/trustpolicyforec2.json
+
+# Embed the permissions policy (in this example an inline policy) to the role to specify what it is allowed to do.
+$ aws iam put-role-policy --role-name Test-Role-for-EC21 --policy-name Permissions-Policy-For-Ec2 --policy-document file://tmp/setup-files/iam-admin-role/permissionspolicyforec2.json
+
+# Create the instance profile required by EC2 to contain the role
+$ aws iam create-instance-profile --instance-profile-name EC2-ListBucket-S31
+
+# Finally, add the role to the instance profile
+$ aws iam add-role-to-instance-profile --instance-profile-name EC2-ListBucket-S31 --role-name Test-Role-for-EC21
